873b50ed87a8b6e660cc4c64ac23a0ceb38c87d9
Hendrik Mans <hendrik@mans.de>
2011-07-21 11:22:37 +0200

    Improve prompt to show indicator when inside a bundle exec shell.

--- hmansify.sh
+++ hmansify.sh
@@ -29,11 +29,14 @@ export PATH=~/hmansify/bin:$PATH
 export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 
 # awesome git-enabled prompt
-function parse_git_branch {
-  ref=$(git-symbolic-ref HEAD 2> /dev/null) || return
-  echo "("${ref#refs/heads/}")" 
+function bundler_prompt {
+  if [ -n "${BUNDLE_GEMFILE:+x}" ]; then
+    echo '[B] '
+  else
+    echo ''
+  fi
 }
-export PS1='\[\033[00;32m\]\u \[\033[00;35m\]\w\[\033[00m\]\[\033[00;33m\]`git branch 2>/dev/null|cut -f2 -d\* -s` \[\033[00m\]\$ '
+export PS1='\[\033[00;32m\]\u \[\033[00;35m\]\w\[\033[00m\]\[\033[00;33m\]`git branch 2>/dev/null|cut -f2 -d\* -s` \[\033[36m\]$(bundler_prompt)\[\033[00m\]\$ '
 
 # memcached wants this
 export EVENT_NOKQUEUE=1
